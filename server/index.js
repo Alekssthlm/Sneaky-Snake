428574440acac2596e144056d54d223b4934abe4
Alexander Heras <alexander.heras@hotmail.com>
2024-03-17 12:16:25 +0100

    Added swagger ui fucntionality

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-
+const swaggerUI = require("swagger-ui-express")  //swagger api
+const swaggerJsDoc = require("swagger-jsdoc")    //swagger api
 const PORT = process.env.PORT ?? 8000;
 
-const allowedOrigins = ['https://sneaky-snake-client.vercel.app']; 
+const options = {          //swagger api
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Sneaky Snake private API",
+      version: "1.0.0",
+      description: "A simple snake API"
+    },
+    servers: [
+      {
+        url: "http://localhost:8000"
+      }
+    ]
+  },
+  apis: ["./index.js"]
+}
 
+const allowedOrigins = ['https://sneaky-snake-client.vercel.app', 'http://localhost:8000/'];   // Allowed urls by CORS
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
@@ -16,8 +32,34 @@ const corsOptions = {
   }
 };
 
+const app = express();
+const specs = swaggerJsDoc(options) //swagger api
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))  //swagger api
 app.use(cors(corsOptions));
 app.use(express.json());
+
+/**
+ * @swagger
+ * tags:
+ *    name: Joke
+ *    description: Joke managing API
+ */
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Get new joke
+ *     tags: [Joke]
+ *     responses:
+ *        200:
+ *         description: Successful response
+ *         content:
+ *             text/plain:
+ *               schema:
+ *                 type: string
+ */
+
 
 app.get('/', async (req, res) => {
   const clientUrl = req.get('referer') || 'Unknown';
